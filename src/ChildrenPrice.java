1ac5fd70acfe12704100666eb9b3da7b85579f47
samslina <avelina.ott@haake.local>
2017-05-03 12:28:54 +0200

    STEP: Replace Conditional with Polymorphism
    The conditional are moved to an overriding method in the subclass.

--- src/ChildrenPrice.java
+++ src/ChildrenPrice.java
@@ -2,4 +2,12 @@ public class ChildrenPrice extends Price {
     int getPriceCode() {
     	return Movie.CHILDRENS;
     }
+    
+    public double getCharge(int daysRented) {
+       double result = 1.5;
+       if (daysRented > 3) {
+    	 result = (daysRented - 3) * 1.5;
+       }
+       return result;
+    }
 }
