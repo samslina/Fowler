a11e6bc10a1d62b31a1c3937343689d8eb1a6d2e
samslina <avelina.ott@haake.local>
2017-05-03 11:53:43 +0200

    STEP: Move method
    STEP: Move method

--- src/Movie.java
+++ src/Movie.java
@@ -4,17 +4,43 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
     private String title;
     private int priceCode;
+   
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
         priceCode = newpriceCode;
     }
+    
     public int getPriceCode() {
+    	
         return priceCode;
     }
     public void setPriceCode(int arg) {
+    	
         priceCode = arg;
     }
+    
     public String getTitle (){
-        return title;
-    };
+        
+    	return title;
+    }
+    
+    public double getCharge(int daysRented){
+    	double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+            	result += 2;
+                if (daysRented > 2)
+                	result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+            	result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+            	result += 1.5;
+                if (daysRented > 3)
+                	result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
 }
