1d0667faace66fb4daa521f8e2b7d8c5530e8cc9
samslina <avelina.ott@haake.local>
2017-05-03 11:23:12 +0200

    STEP: Extract method
    STEP: Extract method

--- src/Customer.java
+++ src/Customer.java
@@ -33,12 +33,8 @@ class Customer {
         while (enum_rentals.hasMoreElements()) {
             Rental each = (Rental) enum_rentals.nextElement();
             //determine amounts for each line
-
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
-                frequentRenterPoints++;
+            frequentRenterPoints  += getFrequentRenterPoints(each);
+            
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getCharge()) + "\n";
             totalAmount += each.getCharge();
@@ -50,5 +46,14 @@ class Customer {
         return result;
     }
 
+	private int getFrequentRenterPoints(Rental each) {
+		if((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1){
+			return 2;
+		}else{
+			return 1;
+		}
+	}
+
+    
 }
     
