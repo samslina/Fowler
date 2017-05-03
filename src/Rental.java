5e0a3be5790e898af422e7d1ae3604fe28307baf
samslina <avelina.ott@haake.local>
2017-05-03 11:55:19 +0200

    getFrequentRenterPoints
    getFrequentRenterPoints

--- src/Rental.java
+++ src/Rental.java
@@ -17,14 +17,11 @@ class Rental {
     }
 
     public double getCharge(){
+    	
     	return this.movie.getCharge(this.daysRented);
     }
     
    public int getFrequentRenterPoints() {
-		if((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1){
-			return 2;
-		}else{
-			return 1;
-		}
+		return this.movie.getFrequentRenterPoints(this.daysRented);
 	}
 }
